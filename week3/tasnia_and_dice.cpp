dd54f6d8884331074e6b30a46857dc7e289be617
Mahbub2001 <imahbubturza@gmail.com>
2023-10-18 23:36:14 +0600

    18/11/2023

---
+++ week3/tasnia_and_dice.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define fill_number(x, n) setfill('0') << setw(n) << (x)
+#define precision_number(x, n) fixed << setprecision(n) << (x)
+template <typename T>
+T lcm(T a, T b)
+{
+    return (a * (b / __gcd(a, b)));
+}
+
+// const long long sieve_N = 2e7;
+// bool is_prime[sieve_N + 1];
+// vector<long long> primes;
+
+// void sieve()
+// {
+//     fill(is_prime, is_prime + sieve_N + 1, true);
+//     is_prime[0] = is_prime[1] = false;
+
+//     for (long long i = 2; i * i <= sieve_N; i++)
+//     {
+//         if (is_prime[i])
+//         {
+//             for (long long j = i * i; j <= sieve_N; j += i)
+//             {
+//                 is_prime[j] = false;
+//             }
+//         }
+//     }
+
+//     for (long long i = 2; i <= sieve_N; i++)
+//     {
+//         if (is_prime[i])
+//         {
+//             primes.push_back(i);
+//         }
+//     }
+// }
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, s, r;
+        cin >> n >> s >> r;
+        vector<int> v(n, 0);
+        v[0] = s - r;
+        for (int i = 1; r != 0; i++)
+        {
+            if (i == n)
+            {
+                i = 1;
+            }
+            v[i]++;
+            r--;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cout << v[i] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
