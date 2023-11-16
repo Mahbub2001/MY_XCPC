4f406727d93ebd94e66b9d0aa4a710e367c1d844
Mahbub2001 <imahbubturza@gmail.com>
2023-11-16 21:31:58 +0600

    16/11/2023

---
+++ week7/B_T_2.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define YES cout << "YES" << en
+#define NO cout << "NO" << en
+#define yes cout << "Yes" << en
+#define no cout << "No" << en
+#define fill_number(x, n) setfill('0') << setw(n) << (x)
+#define precision_number(x, n) fixed << setprecision(n) << (x)
+template <typename T>
+T lcm(T a, T b)
+{
+    return (a * (b / __gcd(a, b)));
+}
+
+// const long long sieve_N = 2e7;
+// bool is_prime[sieve_N + 1];
+// vector<long long> primes;
+
+// void sieve()
+// {
+//     fill(is_prime, is_prime + sieve_N + 1, true);
+//     is_prime[0] = is_prime[1] = false;
+
+//     for (long long i = 2; i * i <= sieve_N; i++)
+//     {
+//         if (is_prime[i])
+//         {
+//             for (long long j = i * i; j <= sieve_N; j += i)
+//             {
+//                 is_prime[j] = false;
+//             }
+//         }
+//     }
+
+//     for (long long i = 2; i <= sieve_N; i++)
+//     {
+//         if (is_prime[i])
+//         {
+//             primes.push_back(i);
+//         }
+//     }
+// }
+
+void getPrime(long long p, unordered_map<long long, long long> &mp)
+{
+    while (p % 2 == 0)
+    {
+        mp[2]++;
+        p /= 2;
+    }
+    for (long long i = 3; i * i <= p; i++)
+    {
+        while (p % i == 0)
+        {
+            mp[i]++;
+            p /= i;
+        }
+    }
+    if (p > 1)
+    {
+        mp[p]++;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        unordered_map<long long, long long> mp;
+        long long n;
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            long long x;
+            cin >> x;
+            getPrime(x, mp);
+        }
+        bool flag = true;
+        for (auto val : mp)
+        {
+            if (val.second % n != 0)
+            {
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+            YES;
+        else
+            NO;
+    }
+
+    return 0;
+}
---
+++ week7/F_T_6.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define YES cout << "YES" << en
+#define NO cout << "NO" << en
+#define yes cout << "Yes" << en
+#define no cout << "No" << en
+#define fill_number(x, n) setfill('0') << setw(n) << (x)
+#define precision_number(x, n) fixed << setprecision(n) << (x)
+template <typename T>
+T lcm(T a, T b)
+{
+    return (a * (b / __gcd(a, b)));
+}
+
+// const long long sieve_N = 2e7;
+// bool is_prime[sieve_N + 1];
+// vector<long long> primes;
+
+// void sieve()
+// {
+//     fill(is_prime, is_prime + sieve_N + 1, true);
+//     is_prime[0] = is_prime[1] = false;
+
+//     for (long long i = 2; i * i <= sieve_N; i++)
+//     {
+//         if (is_prime[i])
+//         {
+//             for (long long j = i * i; j <= sieve_N; j += i)
+//             {
+//                 is_prime[j] = false;
+//             }
+//         }
+//     }
+
+//     for (long long i = 2; i <= sieve_N; i++)
+//     {
+//         if (is_prime[i])
+//         {
+//             primes.push_back(i);
+//         }
+//     }
+// }
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    long long n, k;
+    cin >> n >> k;
+    vector<long long> v;
+    for (long long i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            v.push_back(i);
+            if (n / i != i)
+            {
+                v.push_back(n / i);
+            }
+        }
+    }
+    if (v.size() < k)
+    {
+        cout << -1;
+        return 0;
+    }
+    sort(v.begin(), v.end());
+
+    cout << v[k - 1] << en;
+
+    return 0;
+}
