65fd27ad5f92dca98acdd2e352b6dd5be88471fa
Mahbub2001 <imahbubturza@gmail.com>
2023-11-19 23:19:09 +0600

    19/11/2023

---
+++ Super Zone week 8/G_Grandma_Capa_Knits_a_Scarf.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define YES cout << "YES" << en
+#define NO cout << "NO" << en
+#define yes cout << "Yes" << en
+#define no cout << "No" << en
+#define fill_number(x, n) setfill('0') << setw(n) << (x)
+#define precision_number(x, n) fixed << setprecision(n) << (x)
+template <typename T>
+T lcm(T a, T b)
+{
+    return (a * (b / __gcd(a, b)));
+}
+
+// const long long sieve_N = 2e7;
+// bool is_prime[sieve_N + 1];
+// vector<long long> primes;
+
+// void sieve()
+// {
+//     fill(is_prime, is_prime + sieve_N + 1, true);
+//     is_prime[0] = is_prime[1] = false;
+
+//     for (long long i = 2; i * i <= sieve_N; i++)
+//     {
+//         if (is_prime[i])
+//         {
+//             for (long long j = i * i; j <= sieve_N; j += i)
+//             {
+//                 is_prime[j] = false;
+//             }
+//         }
+//     }
+
+//     for (long long i = 2; i <= sieve_N; i++)
+//     {
+//         if (is_prime[i])
+//         {
+//             primes.push_back(i);
+//         }
+//     }
+// }
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        string s;
+        cin >> n >> s;
+        long long ans = 1e9;
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            long long l = 0, r = n - 1, count = 0;
+            while (l <= r)
+            {
+                if (s[l] == s[r])
+                {
+                    l++;
+                    r--;
+                }
+                else if (s[l] == c)
+                {
+                    l++;
+                    count++;
+                }
+                else if (s[r] == c)
+                {
+                    r--;
+                    count++;
+                }
+                else
+                {
+                    count = 1e9;
+                    break;
+                }
+            }
+            ans = min(ans, count);
+        }
+        if (ans == 1e9)
+        {
+            cout << -1 << en;
+        }
+        else
+        {
+            cout << ans << en;
+        }
+    }
+
+    return 0;
+}
