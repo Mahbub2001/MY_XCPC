25dc78160e41dee7a4fc99b64b36b05e71f1ca6d
Mahbub2001 <imahbubturza@gmail.com>
2023-11-20 22:37:46 +0600

    20/10/2023

---
+++ Super Zone week 8/J_Two_Arrays.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define YES cout << "YES" << en
+#define NO cout << "NO" << en
+#define yes cout << "Yes" << en
+#define no cout << "No" << en
+#define fill_number(x, n) setfill('0') << setw(n) << (x)
+#define precision_number(x, n) fixed << setprecision(n) << (x)
+template <typename T>
+T lcm(T a, T b)
+{
+    return (a * (b / __gcd(a, b)));
+}
+
+// const long long sieve_N = 2e7;
+// bool is_prime[sieve_N + 1];
+// vector<long long> primes;
+
+// void sieve()
+// {
+//     fill(is_prime, is_prime + sieve_N + 1, true);
+//     is_prime[0] = is_prime[1] = false;
+
+//     for (long long i = 2; i * i <= sieve_N; i++)
+//     {
+//         if (is_prime[i])
+//         {
+//             for (long long j = i * i; j <= sieve_N; j += i)
+//             {
+//                 is_prime[j] = false;
+//             }
+//         }
+//     }
+
+//     for (long long i = 2; i <= sieve_N; i++)
+//     {
+//         if (is_prime[i])
+//         {
+//             primes.push_back(i);
+//         }
+//     }
+// }
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        vector<long long> v(n), v1(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v1[i];
+        }
+        sort(v.begin(), v.end());
+        sort(v1.begin(), v1.end());
+        bool flag = true;
+        for (int i = 0; i < n; i++)
+        {
+            if ((v[i] > v1[i]) || (v[i] + 1 < v1[i]))
+            {
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+            YES;
+        else
+            NO;
+    }
+
+    return 0;
+}
